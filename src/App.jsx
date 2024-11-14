ef5f4c0245ad404b3440782b17d7b4cc3210f83e
anishNagula <anish.nagula@gmail.com>
2024-11-14 13:57:07 +0530

    removed cursor animation for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CustomCursor from './components/CustomCursor';
@@ -9,10 +9,25 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Change 768 to your desired breakpoint
+    };
+
+    handleResize(); // Check initial size
+    window.addEventListener('resize', handleResize); // Listen for resize events
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // Cleanup listener on unmount
+    };
+  }, []);
+
   return (
     <Router>
       <div className="main">
-        <CustomCursor />
+        {!isMobile && <CustomCursor />} {/* Render CustomCursor only if not mobile */}
         <Navbar />
         <Routes>
           <Route path="/" element={<About />} />
