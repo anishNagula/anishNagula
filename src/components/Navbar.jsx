b5beb14a7b1c57e9627fedf673065b813f99ba92
anishNagula <anish.nagula@gmail.com>
2024-11-14 12:11:06 +0530

    made site responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from '../styles/navbar.module.css';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  const handleLinkClick = () => {
+    setIsMobile(false);
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.main_left}>
         <h3>AnishNagula</h3>
       </div>
       <div className={styles.main_right}>
-        <ul>
+        <button className={styles.menuToggle} onClick={toggleMenu}>
+          {isMobile ? '✖' : '☰'}
+        </button>
+        <ul className={isMobile ? styles.active : ''}>
           <li>
             <NavLink 
               to="/" 
+              onClick={handleLinkClick}
               className={({ isActive }) => (isActive ? styles.activeLink : '')}
             >
               About
@@ -21,6 +35,7 @@ const Navbar = () => {
           <li>
             <NavLink 
               to="/blog" 
+              onClick={handleLinkClick}
               className={({ isActive }) => (isActive ? styles.activeLink : '')}
             >
               Blog
@@ -29,6 +44,7 @@ const Navbar = () => {
           <li>
             <NavLink 
               to="/projects" 
+              onClick={handleLinkClick}
               className={({ isActive }) => (isActive ? styles.activeLink : '')}
             >
               Projects
@@ -37,6 +53,7 @@ const Navbar = () => {
           <li>
             <NavLink 
               to="/contact" 
+              onClick={handleLinkClick}
               className={({ isActive }) => (isActive ? styles.activeLink : '')}
             >
               Contact
